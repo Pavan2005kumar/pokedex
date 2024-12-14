3d7200de3186e23a80c3b0ac29334c7f7f9f52e7
Pavan2005kumar <pavanbanjara601@gmail.com>
2024-12-14 12:08:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-onst poke_container = document.getElementById('poke-container')
+const poke_container = document.getElementById('poke-container')
 const pokemon_count = 150
 const colors = {
     fire: '#FDDFDF',
